805cb8a764e76ef3aadf9562a29c8845a9ede9f2
busquets <alexis.pointurier@gmail.com>
2018-03-13 16:21:54 +0100

    route debut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from "@angular/platform-browser"
 import { NgModule } from "@angular/core"
 import { HttpClientModule } from "@angular/common/http"
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from "./app.component"
 import { ArticleComponent } from "./article/article.component"
 import { ArticlesComponent } from "./articles/articles.component"
 import { ArticleService } from "./services/article.service";
 
+const appRoutes: Routes = [
+/*  { path: 'create', component: ArticleCreationComponent },*/
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'articles/:id', component: ArticleComponent },
+  { path: '', component: ArticlesComponent }
+]
+
 @NgModule({
   declarations: [AppComponent, ArticleComponent, ArticlesComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    HttpClientModule],
   providers: [ArticleService],
   bootstrap: [AppComponent]
 })
